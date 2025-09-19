a6f3a4d1718086d5cd70d898333691f91f802ca3
paperparrot5010 <me4030290@gmail.com>
2025-09-19 15:42:24 +0300

    Waves system

--- static_body_2d.gd
+++ static_body_2d.gd
@@ -1,20 +1,26 @@
 extends StaticBody2D
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+# A variable to track if the player is in the zone
+var player_in_zone = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animated_sprite_2d.play("Opening")
 	await animated_sprite_2d.animation_finished
 	animated_sprite_2d.play("Working")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	# Check for input every frame, ONLY if the player is in the zone
+	if player_in_zone and Input.is_action_just_pressed("Interact"):
+		print(1)
 
-
+# This function runs when the player enters the zone
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("PlayerGroup"):
-		if Input.is_action_just_pressed("Interact"):
-			print(1)
+		player_in_zone = true # Set the flag to true
+
+# This function runs when the player leaves the zone
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("PlayerGroup"):
+		player_in_zone = false # Set the flag to false
