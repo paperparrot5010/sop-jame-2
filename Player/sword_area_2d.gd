5504d12b71e92b2ed448bf61ba94e85d7b06e5ab
paperparrot5010 <me4030290@gmail.com>
2025-09-19 12:12:04 +0300

    2

--- Player/sword_area_2d.gd
+++ Player/sword_area_2d.gd
@@ -5,7 +5,13 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("shoot"):
 		animation_player.play("Hit")
 
+
+
+
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("EnemyGroup"):
 		print(1)
-		body.take_damage(1)
+		# Call the take_damage function on the enemy
+		if body.has_method("take_damage"):
+			body.take_damage(1) # Assuming 1 damage per bullet
