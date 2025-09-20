8b7463d53c0afb47a19169f61aa8640e7d8728bf
paperparrot5010 <me4030290@gmail.com>
2025-09-20 13:59:25 +0300

    Stability

--- static_body_2d.gd
+++ static_body_2d.gd
@@ -3,11 +3,14 @@ extends StaticBody2D
 @onready var keyboard_letters_and_symbols: Sprite2D = $KeyboardLettersAndSymbols
 @export var control_node: Node
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@export var stability = 100
+@onready var timer: Timer = $Timer
 
 # A variable to track if the player is in the zone
 var player_in_zone = false
 
 func _ready() -> void:
+	timer.start()
 	control_node = get_tree().get_first_node_in_group("ControlGroup")
 	keyboard_letters_and_symbols.hide()
 	animated_sprite_2d.play("Opening")
@@ -17,7 +20,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	# Check for input every frame, ONLY if the player is in the zone
 	if player_in_zone and Input.is_action_just_pressed("Interact"):
-		add_child(control_node)
+		print(11)
 
 # This function runs when the player enters the zone
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -31,3 +34,8 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group("PlayerGroup"):
 		animation_player.play("vanish")
 		player_in_zone = false # Set the flag to false
+
+
+func _on_timer_timeout() -> void:
+	stability -= 1
+	print(stability)
